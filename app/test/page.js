8985ecf8dfb01bd8f58753c8d38f542d7a3acf89
Kerolos Morkos <135118128+Kerolos17@users.noreply.github.com>
2024-12-26 18:50:22 +0200

    ...

--- app/test/page.js
+++ app/test/page.js
@@ -5,7 +5,10 @@ const page = () => {
   return (
     <div className="mt-[10em]">
         <h1>test</h1>
-        <Image src="/assets/images/img-1.png" width={100} height={100}/>
+        <Image src="/assets/images/img-1.png" width={100} height={100} alt="Description of image"/>
+        <Image src="/path/to/image.jpg" alt="Description of image" width={500} height={300} />
+        <Image src="/path/to/another-image.jpg" alt="Description of another image" width={500} height={300} />
+        <p>It&apos;s important to escape single quotes in JSX.</p>
     </div>
   )
 }
